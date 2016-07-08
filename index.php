7f064db90b6d7e8c722488a96334b5a329cdb26a
lpz <lpz2@163.com>
2016-07-08 17:31:17 +0800

    使用wordpress保留的acticle标签定义css样式

--- index.php
+++ index.php
@@ -4,9 +4,10 @@ if (have_posts()):
 	while (have_posts()) :
 		the_post();
 ?>
-		<h2><a href="<?php the_permalink()?>"><?php the_title() ?></a></h2>
-		<h2><?php the_content( )?></h2>
-		
+		<article class="yyy">
+			<h2><a href="<?php the_permalink()?>"><?php the_title() ?></a></h2>
+			<h2><?php the_content( )?></h2>
+		</article>
 <?php		
 	endwhile;
 else:
