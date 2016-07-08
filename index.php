165ebc0e5246edd401d9e2a0ea479f7bfbec06f1
lpz <lpz2@163.com>
2016-07-08 16:29:15 +0800

    首页可以显示文章列表

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php 
 
-echo "hello world"; ?>
+if (have_posts()) {
+	while (have_posts()) {
+		the_post();
+		echo "<h2>".the_title()."</h2>";
+		echo "<h2>".the_content( )."</h2>";
+		
+	}
+}else{
+	echo "<p>没有文章</p>";
+}
+
+?>
