3c8a3ff7565a12df9b74777133304cb745572b2b
lpz <lpz2@163.com>
2016-07-08 23:11:10 +0800

    用模板页和页面用后台设置的方式结合，先要写好模板页的注释部分，后台才看得到

---
+++ special-template.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: 特别布局
+*/ 
+get_header( );
+if (have_posts()):
+	while (have_posts()) :
+		the_post();
+?>
+		<article class="page">
+			<h2><a href="<?php the_permalink()?>"><?php the_title() ?></a></h2>
+			<h2><?php the_content( )?></h2>
+		</article>
+<?php		
+	endwhile;
+else:
+	echo "<p>没有文章</p>";
+endif;
+
+get_footer();
+?>
