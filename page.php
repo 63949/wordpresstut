dd57c8e04eefe10a427c4948af344ed4dd9443e9
lpz <lpz2@163.com>
2016-07-08 22:31:07 +0800

    建立全局页面,使用css控制页面格式

---
+++ page.php
@@ -0,0 +1,18 @@
+<?php 
+get_header( );
+if (have_posts()):
+	while (have_posts()) :
+		the_post();
+?>
+		<article class="page">
+			<h2><a href="<?php the_permalink()?>"><?php the_title() ?></a></h2>
+			<h2><?php the_content( )?></h2>
+		</article>
+<?php		
+	endwhile;
+else:
+	echo "<p>没有文章</p>";
+endif;
+
+get_footer();
+?>
