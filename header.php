d2d1e9093aab148a5c60c4a4b3115ddc68283474
lpz <lpz2@163.com>
2016-07-08 16:43:05 +0800

    添加网页头部和尾部

---
+++ header.php
@@ -0,0 +1,16 @@
+<html <?php language_attributes( ); ?>>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<meta charset="<?php bloginfo('charset' ); ?>"></meta>
+<meta name="viewport" content='width=device-width'>
+	<title><?php bloginfo('name' ); ?></title>
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class( ); ?>>
+<!-- site-header -->
+<header class="site-header">
+	<h1><?php bloginfo('name' ); ?></h1>
+	<h5><?php bloginfo('description' ); ?></h5>
+</header><!-- site-header -->
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php 
-
+get_header( );
 if (have_posts()) {
 	while (have_posts()) {
 		the_post();
 		echo "<h2>".the_title()."</h2>";
 		echo "<h2>".the_content( )."</h2>";
+		echo "<h2>".the_permalink()."</h2>";
 		
 	}
 }else{
 	echo "<p>没有文章</p>";
 }
-
+get_footer();
 ?>
